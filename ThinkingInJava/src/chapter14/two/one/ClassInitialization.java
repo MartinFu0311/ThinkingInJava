92409f8996399ef0df9c71b1c939b256eccc7fe3
fxb248 <fxb248@126.com>
2015-12-11 02:43:32 -0500

    modify ClassInitialization because we use forName

--- ThinkingInJava/src/chapter14/two/one/ClassInitialization.java
+++ ThinkingInJava/src/chapter14/two/one/ClassInitialization.java
@@ -15,7 +15,7 @@ public class ClassInitialization {
 		
 		System.out.println(Initable2.staticNonFinal);  //static final值是编译期常量，但这个只是static不是final，所以需要引发初始化，需要分配空间
 		
-		Class<?> initable3 = Class.forName("chapter9.two.one.Initable3");  //引发初始化
+		Class<?> initable3 = Class.forName("chapter14.two.one.Initable3");  //引发初始化
 		System.out.println("After creating Initable3 ref");
 		System.out.println(Initable3.staticNonFinal);
 	}
